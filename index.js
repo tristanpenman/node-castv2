79b13cfd659c2053b3d0d9a9cd4c88dbfc92cad6
Tristan Penman <tristan@tristanpenman.com>
2022-03-08 16:47:32 +1100

    Support for advertising via mdns

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const Advertisement = require('./lib/advertisement');
 const Client = require('./lib/client');
 const Device = require('./lib/device');
 const Server = require('./lib/server');
 
-module.exports.Client = Client;
-module.exports.Device = Device;
-module.exports.Server = Server;
+module.exports = {
+  Advertisement,
+  Client,
+  Device,
+  Server
+};
